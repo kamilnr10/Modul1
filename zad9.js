9fc8fa9cc0eef0c3f1ac085ef284fdc29c42ae00
Kamilnr10 <kamil.nr10@gmail.com>
2020-01-20 18:40:56 +0100

    zadanie 9

--- zad9.js
+++ zad9.js
@@ -5,14 +5,14 @@
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'y', 'z']
 const polishAlphabet = ['ą', 'ć', 'ę', 'ó', 'ś', 'ź']
 
-function forIn(array, forFn) {
-    for (let prop in alphabet) {
-        console.log(`${prop} : ${alphabet[prop]}`);
-    }
-}
+// function forIn(array, forFn) {
+//     for (let prop in alphabet) {
+//         console.log(`${prop} : ${alphabet[prop]}`);
+//     }
+// }
 
 const mapForInArr = alphabet.map((value, index) => {
-    // console.log(`${index} : ${value}`)
+    console.log(`${index} : ${value}`)
     return {
         index,
         value
@@ -20,21 +20,21 @@ const mapForInArr = alphabet.map((value, index) => {
 })
 
 
-function forOf(array, forFn) {
-    for (let alpha of polishAlphabet) {
-        console.log(`${alpha}`)
-    }
-}
+// function forOf(array, forFn) {
+//     for (let alpha of polishAlphabet) {
+//         console.log(`${alpha}`)
+//     }
+// }
 
-function mapForOf() {
-    polishAlphabet.map((value) => {
-        console.log(value)
-    })
-}
+// function mapForOf() {
+//     polishAlphabet.map((value) => {
+//         console.log(value)
+//     })
+// }
 
 
-function forI(array, forFn) {
-    for (let i = 0; i < polishAlphabet.length; i++) {
-        console.log(polishAlphabet[i])
-    }
-}
+// function forI(array, forFn) {
+//     for (let i = 0; i < polishAlphabet.length; i++) {
+//         console.log(polishAlphabet[i])
+//     }
+// }
