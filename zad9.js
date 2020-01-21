88dd20219322290b397f641eae90d046986e9512
Kamilnr10 <kamil.nr10@gmail.com>
2020-01-21 17:52:34 +0100

    zadanie 9

--- zad9.js
+++ zad9.js
@@ -1,40 +1,70 @@
 // stwórz sobie array z literami polskiego alfabetu
-// stwórz kod na różne metody for, które istnieją w JS 
-// a potem postaraj się za pomocą funkcji .map odtworzyć działanie każdej funkcji for						
+// stwórz kod na różne metody for, które istnieją w JS
+// a potem postaraj się za pomocą funkcji .map odtworzyć działanie każdej funkcji for
 
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'y', 'z']
-const polishAlphabet = ['ą', 'ć', 'ę', 'ó', 'ś', 'ź']
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "r",
+  "s",
+  "t",
+  "u",
+  "w",
+  "y",
+  "z"
+];
+const polishAlphabet = ["ą", "ć", "ę", "ó", "ś", "ź"];
 
 function forIn(array, forFn) {
-    for (let prop in alphabet) {
-        console.log(`${prop} : ${alphabet[prop]}`);
-    }
+  for (let prop in alphabet) {
+    console.log(`${prop} : ${alphabet[prop]}`);
+  }
 }
 
 const mapForInArr = alphabet.map((value, index) => {
-    console.log(`${index} : ${value}`)
-    return {
-        index,
-        value
-    }
-})
-
+  console.log(`${index} : ${value}`);
+  return {
+    index,
+    value
+  };
+});
 
 function forOf(array, forFn) {
-    for (let alpha of polishAlphabet) {
-        console.log(`${alpha}`)
-    }
+  for (let alpha of polishAlphabet) {
+    console.log(`${alpha}`);
+  }
 }
+
+const mapForOfArr = polishAlphabet.map((value, index) => {
+  console.log(`${value}`);
+});
 
 function mapForOf() {
-    polishAlphabet.map((value) => {
-        console.log(value)
-    })
+  polishAlphabet.map(value => {
+    console.log(value);
+  });
 }
-
 
 function forI(array, forFn) {
-    for (let i = 0; i < polishAlphabet.length; i++) {
-        console.log(polishAlphabet[i])
-    }
+  for (let i = 0; i < polishAlphabet.length; i++) {
+    console.log(`${polishAlphabet[i]}: ${i}`);
+  }
 }
+
+const mapForIArr = polishAlphabet.map((value, index) => {
+  console.log(`${value}: ${index}`);
+});
