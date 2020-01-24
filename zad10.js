78a63731442392b1ba020781257b7b268f678138
Kamilnr10 <kamil.nr10@gmail.com>
2020-01-24 18:03:27 +0100

    zadanie 10...

--- zad10.js
+++ zad10.js
@@ -1,15 +1,44 @@
-const polishAlphabet = ["ą", "ć", "ę", "ó", "ś", "ź"];
+// korzystając z funkcji .reduce stwórz agregację liter alfabetu
+//  do postacji array of arrays,
+//  tak aby losowo 4-7 liczb stanowiło jeden array 
+// - grupa tych arrayów ma być wynikiem tego zadania							
+
+const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "r",
+    "s",
+    "t",
+    "u",
+    "w",
+    "y",
+    "z"
+];
 
 
+const shuffleArr = (arr) => {
+    const randomNumber = Math.floor(Math.random() * alphabet.length);
+    console.log(randomNumber)
 
-const emptyArr = [];
-const agregate = () => {
-    const index = Math.floor(Math.random() * polishAlphabet.length);
-    if (index > 0) {
-        const arr1 = polishAlphabet.splice(0, index);
-        const arr2 = polishAlphabet.splice(0, polishAlphabet.length)
-        emptyArr.push(arr1, arr2);
-        return emptyArr
+    const dividedArray = [];
+    for (let i = 0; i < arr.length; i += randomNumber) {
+        dividedArray.push(arr.slice(i, randomNumber + i));
     }
-
+    return dividedArray;
 }
+
+console.log(shuffleArr(alphabet))
