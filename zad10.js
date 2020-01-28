e8912dcffbc8ce63d98546d89ffeb56a62ec20e2
Kamilnr10 <kamil.nr10@gmail.com>
2020-01-28 12:56:08 +0100

    zadanie 10

--- zad10.js
+++ zad10.js
@@ -30,46 +30,15 @@ const alphabet = [
 ];
 
 
-const shuffleArr = (arr) => {
-    const randomNumber = Math.floor(Math.random() * alphabet.length);
+const chunkArray = (arr) => {
+    const randomNumber = Math.floor(Math.random() * (8 - 4)) + 4;
     console.log(randomNumber)
-
     const dividedArray = [];
     for (let i = 0; i < arr.length; i += randomNumber) {
-        dividedArray.push(arr.slice(i, randomNumber + i));
+        const rnd = Math.floor(Math.random() * (8 - 4)) + 4;
+        dividedArray.push(arr.slice(i, rnd + i));
     }
     return dividedArray;
 }
 
-// console.log(shuffleArr(alphabet))
-
-
-const nextIndex = () => {
-    return Math.floor(Math.random() * (8 - 4)) + 4;
-}
-
-const chunk = (array) => {
-    const dividedArr = [];
-    for (let i = 0; i < array.length; i++) {
-        const part = dividedArr[dividedArr.length - 1];
-        if (!part || part.length === Math.floor(Math.random() * (7 - 4 + 1) + 4)) {
-            dividedArr.push([array[i]]);
-        } else {
-            part.push(array[i]);
-        }
-    }
-    return dividedArr;
-}
-
-
-// stwórz sobie zmieną nextIndex która na początku jest 1 liczbą random
-// 6:06
-// a potem dodawaj do niej kolejne liczby randomowe jeśli przepchnie się cały array
-// 6:06
-// powinieneś wewnątrz fora użyć ifa
-// 6:06
-// i stworzyć sobie np let partArray = []
-// 6:06
-// do którego są wrzucane elementy w danej częsci
-// 6:07
-// lub jeśli index i nextIndex będą się zgadzać to partArray powinien być wrzucany do dividedArray
+console.log(chunkArray(alphabet))
