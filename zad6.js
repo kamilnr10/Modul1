cd4d3ddeac5df77547d678a1a9f98fee8549dac2
Kamilnr10 <kamil.nr10@gmail.com>
2020-02-05 19:08:17 +0100

    Task 1-11 functional programming

--- zad6.js
+++ zad6.js
@@ -1,9 +1,11 @@
 // 6) Masz daną tablicę: const numbers = [2, 5, 7, 10, 34, 16, 879, 1].
 //  Napisz funkcję, która zwróci nową tablicę zawierającą tylko parzyste liczby z tablicy numbers.
 const numbers = [2, 5, 7, 10, 34, 16, 879, 1];
-const isEvenNumbers = array => {
+const filterEvenNumbers = array => {
   return array.filter(el => {
     return el % 2 == 0;
   });
 };
-console.log(`Task 6: Only even numbers in array: ${isEvenNumbers(numbers)} `);
+console.log(
+  `Task 6: Only even numbers in array: ${filterEvenNumbers(numbers)} `
+);
